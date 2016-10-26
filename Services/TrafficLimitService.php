be7145b802ae299fc4d5ed6144b355a252b58803
romeritoCL <romero.latorre@gmail.com>
2016-10-26 10:37:46 +0200

    fixed configuration to create services

--- Services/TrafficLimitService.php
+++ Services/TrafficLimitService.php
@@ -2,23 +2,45 @@
 
 namespace Devoralive\TrafficLimit\Services;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 
+/**
+ * Class TrafficLimitService
+ *
+ * @package Devoralive\TrafficLimit\Services
+ */
 class TrafficLimitService
 {
-
-    public function __construct($enabled, $redisDsn)
+    /**
+     * TrafficLimitService constructor.
+     *
+     * @param $enabled
+     * @param $sncClient
+     * @param $amount
+     * @param $ttl
+     * @param $serviceName
+     */
+    public function __construct($enabled, $sncClient, $amount, $ttl, $serviceName)
     {
         $this->enabled = $enabled;
-        $this->redisDsn = $redisDsn;
+        $this->sncClient = $sncClient;
+        $this->amount = $amount;
+        $this->ttl = $ttl;
+        $this->serviceName = $serviceName;
     }
 
     /**
-     * Redis DSN
-     *
-     * @var string
+     * @var ContainerInterface
      */
-    protected $redisDsn;
+    protected $container;
+
+    /**
+     * SncRedis Client
+     *
+     * @var \Snc\RedisBundle\Client\Phpredis\Client
+     */
+    protected $sncClient;
 
     /**
      * Enable/Disable Service
@@ -28,12 +50,38 @@ class TrafficLimitService
     protected $enabled;
 
     /**
+     * Amount of request allowed
+     *
+     * @var int
+     */
+    protected $amount;
+
+    /**
+     * Amount of time to expire a request
+     *
+     * @var int
+     */
+    protected $ttl;
+
+    /**
+     * Registered Service name
+     *
+     * @var string
+     */
+    protected $serviceName;
+
+    /**
+     * @var \Snc\RedisBundle\Client\Phpredis\Client
+     */
+    protected $sncService;
+    
+    /**
      * Throw new exception
      *
      * @throws TooManyRequestsHttpException
      */
     public function addRequest()
     {
-        throw new TooManyRequestsHttpException('Too many request limit');
+
     }
 }
